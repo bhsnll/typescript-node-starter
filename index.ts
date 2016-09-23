0148bdcb9d0284783bc99639e07df986ae25ffd6
Braden Snell <bhsnll@gmail.com>
2016-09-22 22:52:45 -0600

    initial commit

---
+++ index.ts
@@ -0,0 +1,4 @@
+import { TypescriptNodeStarter } from './src/TypescriptNodeStarter';
+
+const starter = new TypescriptNodeStarter();
+starter.sayHi();
---
+++ src/TypescriptNodeStarter.ts
@@ -0,0 +1,5 @@
+export class TypescriptNodeStarter {
+  public sayHi() {
+    console.log('Typescript Node Starter');
+  }
+}
